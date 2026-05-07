5360a1ba71834c8c69fe82a7d240183e0205687d
Matthew Lese <matthewjlese@gmail.com>
2026-05-07 13:55:38 -0700

    uses instance? checks in tests, :each fixtures for simplicity

--- test/easy_onnx/runtime_test.clj
+++ test/easy_onnx/runtime_test.clj
@@ -1,7 +1,8 @@
 (ns easy-onnx.runtime-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [com.stuartsierra.component :as component]
-            [easy-onnx.runtime :as runtime]))
+            [easy-onnx.runtime :as runtime])
+  (:import [ai.onnxruntime OrtEnvironment OrtSession]))
 
 (def model-path "resources/ml/all-MiniLM-L6-v2/model.onnx")
 
@@ -10,54 +11,49 @@
 
 (def ^:dynamic *fixtures?* false)
 
-(use-fixtures :once
-  (fn [f]
-    (binding [*fixtures?* (fixtures-present?)]
-      (when-not *fixtures?*
-        (println "Skipping easy-onnx.runtime-test: missing model fixture at"
-                 model-path))
-      (f))))
+(use-fixtures :each (fn [f]
+                      (binding [*fixtures?* (fixtures-present?)]
+                        (when-not *fixtures?*
+                          (println "Skipping easy-onnx.runtime-test: missing model fixture at"
+                                   model-path))
+                        (f))))
 
 (deftest create-returns-started-session
   (testing "create returns a Session with env and session populated"
-    (when *fixtures?*
-      (with-open [s (runtime/create {:model-path model-path})]
-        (is (some? (:env s)))
-        (is (some? (:session s)))))))
+    (with-open [s (runtime/create {:model-path model-path})]
+      (is (instance? OrtEnvironment (:env s)))
+      (is (instance? OrtSession (:session s))))))
 
 (deftest run-model-returns-output-map
   (testing "run-model returns a map keyed by output name"
-    (when *fixtures?*
-      (with-open [s (runtime/create {:model-path model-path})]
-        (let [inputs {"input_ids" (into-array [(long-array [101 7592 102])])
-                      "attention_mask" (into-array [(long-array [1 1 1])])
-                      "token_type_ids" (into-array [(long-array [0 0 0])])}
-              outputs (runtime/run-model s inputs)]
-          (is (contains? outputs "last_hidden_state")))))))
+    (with-open [s (runtime/create {:model-path model-path})]
+      (let [inputs {"input_ids" (into-array [(long-array [101 7592 102])])
+                    "attention_mask" (into-array [(long-array [1 1 1])])
+                    "token_type_ids" (into-array [(long-array [0 0 0])])}
+            outputs (runtime/run-model s inputs)]
+        (is (contains? outputs "last_hidden_state"))))))
 
 (deftest with-open-closes-session
   (testing "after with-open exits, the underlying OrtSession is closed"
-    (when *fixtures?*
-      (let [s (runtime/create {:model-path model-path})
-            raw (:session s)]
-        (.close s)
-        (is (thrown? Exception
-                     (runtime/run-model
-                      {:env (:env s) :session raw}
-                      {"input_ids" (into-array [(long-array [101])])
-                       "attention_mask" (into-array [(long-array [1])])
-                       "token_type_ids" (into-array [(long-array [0])])})))))))
+    (let [s (runtime/create {:model-path model-path})
+          raw (:session s)]
+      (.close s)
+      (is (thrown? Exception
+                   (runtime/run-model
+                    {:env (:env s) :session raw}
+                    {"input_ids" (into-array [(long-array [101])])
+                     "attention_mask" (into-array [(long-array [1])])
+                     "token_type_ids" (into-array [(long-array [0])])}))))))
 
 (deftest component-lifecycle
   (testing "component returns an unstarted Session that start populates"
-    (when *fixtures?*
-      (let [c (runtime/component {:model-path model-path})]
-        (is (nil? (:session c)) "unstarted has no session")
-        (let [started (component/start c)]
-          (is (some? (:env started)))
-          (is (some? (:session started)))
-          (let [stopped (component/stop started)]
-            (is (nil? (:session stopped)) "stopped has no session")))))))
+    (let [c (runtime/component {:model-path model-path})]
+      (is (nil? (:session c)) "unstarted has no session")
+      (let [started (component/start c)]
+        (is (instance? OrtEnvironment (:env started)))
+        (is (instance? OrtSession (:session started)))
+        (let [stopped (component/stop started)]
+          (is (nil? (:session stopped)) "stopped has no session"))))))
 
 (deftest init-exception-propagates
   (testing "create with a nonexistent model path throws"
--- test/easy_onnx/tokenizer_test.clj
+++ test/easy_onnx/tokenizer_test.clj
@@ -1,7 +1,8 @@
 (ns easy-onnx.tokenizer-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [com.stuartsierra.component :as component]
-            [easy-onnx.tokenizer :as tokenizer]))
+            [easy-onnx.tokenizer :as tokenizer])
+  (:import [ai.djl.huggingface.tokenizers HuggingFaceTokenizer]))
 
 (def tokenizer-path "resources/ml/all-MiniLM-L6-v2/tokenizer.json")
 
@@ -10,39 +11,35 @@
 
 (def ^:dynamic *fixtures?* false)
 
-(use-fixtures :once
-  (fn [f]
-    (binding [*fixtures?* (fixtures-present?)]
-      (when-not *fixtures?*
-        (println "Skipping easy-onnx.tokenizer-test: missing fixture at"
-                 tokenizer-path))
-      (f))))
+(use-fixtures :each (fn [f]
+                      (binding [*fixtures?* (fixtures-present?)]
+                        (when-not *fixtures?*
+                          (println "Skipping easy-onnx.tokenizer-test: missing fixture at"
+                                   tokenizer-path))
+                        (f))))
 
 (deftest create-returns-started-tokenizer
   (testing "create returns a Tokenizer with the underlying tokenizer set"
-    (when *fixtures?*
-      (with-open [t (tokenizer/create {:tokenizer-path tokenizer-path})]
-        (is (some? (:tokenizer t)))))))
+    (with-open [t (tokenizer/create {:tokenizer-path tokenizer-path})]
+      (is (instance? HuggingFaceTokenizer (:tokenizer t))))))
 
 (deftest encode-returns-encoding-with-ids-and-mask
   (testing "encode returns an Encoding with non-empty ids and mask"
-    (when *fixtures?*
-      (with-open [t (tokenizer/create {:tokenizer-path tokenizer-path})]
-        (let [enc (tokenizer/encode t "hello world")
-              ids (tokenizer/get-ids enc)
-              mask (tokenizer/get-mask enc)]
-          (is (pos? (alength ^longs ids)))
-          (is (= (alength ^longs ids) (alength ^longs mask))))))))
+    (with-open [t (tokenizer/create {:tokenizer-path tokenizer-path})]
+      (let [enc (tokenizer/encode t "hello world")
+            ids (tokenizer/get-ids enc)
+            mask (tokenizer/get-mask enc)]
+        (is (pos? (alength ^longs ids)))
+        (is (= (alength ^longs ids) (alength ^longs mask)))))))
 
 (deftest component-lifecycle
   (testing "component returns an unstarted Tokenizer that start populates"
-    (when *fixtures?*
-      (let [c (tokenizer/component {:tokenizer-path tokenizer-path})]
-        (is (nil? (:tokenizer c)) "unstarted has no tokenizer")
-        (let [started (component/start c)]
-          (is (some? (:tokenizer started)))
-          (let [stopped (component/stop started)]
-            (is (nil? (:tokenizer stopped)) "stopped has no tokenizer")))))))
+    (let [c (tokenizer/component {:tokenizer-path tokenizer-path})]
+      (is (nil? (:tokenizer c)) "unstarted has no tokenizer")
+      (let [started (component/start c)]
+        (is (instance? HuggingFaceTokenizer (:tokenizer started)))
+        (let [stopped (component/stop started)]
+          (is (nil? (:tokenizer stopped)) "stopped has no tokenizer"))))))
 
 (deftest init-exception-propagates
   (testing "create with a nonexistent tokenizer path throws"
