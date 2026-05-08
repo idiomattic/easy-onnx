eaafab2724c79e61186d52d645802cda67fdff48
Matthew Lese <matthewjlese@gmail.com>
2026-05-08 15:09:30 -0700

    updates build

--- build.clj
+++ build.clj
@@ -4,17 +4,16 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'net.clojars.easy-onnx/easy-onnx)
-(def version "0.1.0-SNAPSHOT")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
-  (let [basis    (b/create-basis {:aliases [:test]})
-        cmds     (b/java-command
-                  {:basis      basis
-                    :main      'clojure.main
-                    :main-args ["-m" "cognitect.test-runner"]})
+  (let [basis (b/create-basis {:aliases [:test]})
+        cmds (b/java-command
+              {:basis basis
+               :java-opts ["--enable-native-access=ALL-UNNAMED"]
+               :main 'clojure.main
+               :main-args ["-m" "kaocha.runner"]})
         {:keys [exit]} (b/process cmds)]
     (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
   opts)
@@ -37,13 +36,13 @@
 
 (defn- jar-opts [opts]
   (assoc opts
-          :lib lib   :version version
-          :jar-file  (format "target/%s-%s.jar" lib version)
-          :basis     (b/create-basis {})
-          :class-dir class-dir
-          :target    "target"
-          :src-dirs  ["src"]
-          :pom-data  (pom-template version)))
+         :lib lib :version version
+         :jar-file (format "target/%s-%s.jar" lib version)
+         :basis (b/create-basis {})
+         :class-dir class-dir
+         :target "target"
+         :src-dirs ["src"]
+         :pom-data (pom-template version)))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
   (test opts)
